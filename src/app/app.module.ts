a8d59074eee4b46207738c7e18539a7a6039b47f
Tribhuwan Dewangan <dewangan.tribhuwan@gmail.com>
2023-06-25 11:09:05 +0530

    added clarity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { SharedModule } from '@shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from '@shared/shared.module';
 import { ShellComponent } from './layout/shell/shell.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { SidenavComponent } from './layout/sidenav/sidenav.component';
@@ -17,6 +19,7 @@ import { SidenavComponent } from './layout/sidenav/sidenav.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     SharedModule
   ],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClarityModule } from '@clr/angular';
 
+
+const modules: any[] = [
+  ClarityModule
+]
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ...modules
+  ],
+  exports: [
+    ...modules
   ]
 })
 export class SharedModule { }
