aadfe30a304297c8501f5b8cc5120a0aaf81f514
Tribhuwan Dewangan <dewangan.tribhuwan@gmail.com>
2023-06-25 10:54:58 +0530

    added shell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from '@shared/shared.module';
+import { ShellComponent } from './layout/shell/shell.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { SidenavComponent } from './layout/sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShellComponent,
+    NavbarComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
